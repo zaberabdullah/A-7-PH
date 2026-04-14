ca4692fcd0d9e5efd472287c3fef626aeafb8653
zaberabdullah <zaberabdullah120@gmail.com>
2026-04-15 00:41:03 +0600

    structure the folders before start

---
+++ src/app/components/FriendCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FriendCard = () => {
+    return (
+        <div>
+            <h2>Cards will be here.....</h2>
+        </div>
+    );
+};
+
+export default FriendCard;
